d03dba27352b13dc9211bb288d6f390ef217f316
Emy sheta <emysheta1682@gmail.com>
2023-09-27 00:20:30 +0200

    initial project. added empty home screen.

---
+++ lib/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'heme-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
