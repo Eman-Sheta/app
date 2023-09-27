c090a504103895598d33aa880407766e7280dadf
Emy sheta <emysheta1682@gmail.com>
2023-09-27 22:43:44 +0200

    added settings tab.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:app_islami/hadeth/hadeth_details.dart';
 import 'package:app_islami/home_screen.dart';
 import 'package:app_islami/my_theme.dart';
+import 'package:app_islami/provider/app_config_provider.dart';
 import 'package:app_islami/quran/sura_details.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppConfigProvider(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: Locale(provider.appLanguage),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
@@ -20,6 +28,8 @@ class MyApp extends StatelessWidget {
         Hadethdetails.routeName: (context) => Hadethdetails(),
       },
       theme: MyTheme.lightMode,
+      themeMode: provider.appTheme,
+      darkTheme: MyTheme.darkMode,
     );
   }
 }
--- lib/quran/sura_details.dart
+++ lib/quran/sura_details.dart
@@ -1,6 +1,9 @@
+import 'package:app_islami/my_theme.dart';
+import 'package:app_islami/provider/app_config_provider.dart';
 import 'package:app_islami/quran/item_sura_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 class Suradetails extends StatefulWidget {
   static const String routeName = 'sura_screen';
@@ -15,16 +18,24 @@ class _SuradetailsState extends State<Suradetails> {
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context)?.settings.arguments as SuraDetailsArgs;
+    var provider = Provider.of<AppConfigProvider>(context);
     if (verses.isEmpty) {
       loadFile(args.index);
     }
     return Stack(children: [
-      Image.asset(
-        'assets/images/background_light.png',
-        width: double.infinity,
-        height: double.infinity,
-        fit: BoxFit.fill,
-      ),
+      provider.isDarkMode()
+          ? Image.asset(
+              'assets/images/background_dark.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fill,
+            )
+          : Image.asset(
+              'assets/images/background_light.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fill,
+            ),
       Scaffold(
           appBar: AppBar(
             title: Text(
@@ -34,36 +45,61 @@ class _SuradetailsState extends State<Suradetails> {
           ),
           body: verses.length == 0
               ? Center(child: CircularProgressIndicator())
-              : Container(
-                  margin: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * 0.05,
-                      vertical: MediaQuery.of(context).size.height * 0.06),
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24)),
-                  child: ListView.separated(
-                    separatorBuilder: (context, index) {
-                      return Divider(
-                        color: Theme.of(context).primaryColor,
-                        thickness: 1,
-                      );
-                    },
-                    itemBuilder: ((context, index) {
-                      return ItemSuraDetails(
-                        content: verses[index],
-                        index: index,
-                      );
-                    }),
-                    itemCount: verses.length,
-                  ),
-                ))
+              : provider.isDarkMode()
+                  ? Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.05,
+                          vertical: MediaQuery.of(context).size.height * 0.06),
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: MyTheme.primaryDark,
+                          borderRadius: BorderRadius.circular(24)),
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) {
+                          return Divider(
+                            color: Theme.of(context).primaryColor,
+                            thickness: 1,
+                          );
+                        },
+                        itemBuilder: ((context, index) {
+                          return ItemSuraDetails(
+                            content: verses[index],
+                            index: index,
+                          );
+                        }),
+                        itemCount: verses.length,
+                      ),
+                    )
+                  : Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.05,
+                          vertical: MediaQuery.of(context).size.height * 0.06),
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(24)),
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) {
+                          return Divider(
+                            color: Theme.of(context).primaryColor,
+                  thickness: 1,
+                );
+              },
+              itemBuilder: ((context, index) {
+                return ItemSuraDetails(
+                  content: verses[index],
+                  index: index,
+                );
+              }),
+              itemCount: verses.length,
+            ),
+          ))
     ]);
   }
 
   void loadFile(int index) async {
     String content =
-        await rootBundle.loadString('assets/files/${index + 1}.txt');
+    await rootBundle.loadString('assets/files/${index + 1}.txt');
     List<String> lines = content.split('\n');
     verses = lines;
     setState(() {});
